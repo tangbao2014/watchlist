99692830dca3f13c78bca246f9ecc3817d6b784a
tangbao2014 <48447880@qq.com>
2019-03-13 10:24:09 +0800

    add static files

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
-from flask import Flask,url_for
+from flask import Flask,url_for,render_template
 app = Flask(__name__)
 
+name = 'tangbao'
+
+movies = [
+    {'name': 'My Neighbor Totoro', 'year': '1988'},
+    {'name': 'Three Colours trilogy', 'year': '1993'},
+    {'name': 'Forrest Gump', 'year': '1994'},
+    {'name': 'Perfect Blue', 'year': '1997'},
+    {'name': 'The Matrix', 'year': '1999'},
+    {'name': 'Memento', 'year': '2000'},
+    {'name': 'The Bucket list', 'year': '2007'},
+    {'name': 'Black Swan', 'year': '2010'},
+    {'name': 'Gone Girl', 'year': '2014'},
+    {'name': 'CoCo', 'year': '2017'},
+]
+
 @app.route('/')
-def hello():
-    return "Welocme to My Watchlist!"
+def index():
+    return render_template('index.html', name=name, movies=movies)
 
 @app.route('/user/<name>')
 def user_page(name):
